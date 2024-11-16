1546456614e06cf5de114bc6d0dd614334a68c57
*nhson26122004* <*nhson26122004@gmail.com*>
2024-11-16 16:47:34 +0700

    simple authentication

--- src/main/java/com/nhson/demo11/LedController.java
+++ src/main/java/com/nhson/demo11/LedController.java
@@ -1,6 +1,8 @@
 package com.nhson.demo11;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
@@ -17,29 +19,50 @@ public class LedController {
     @Autowired
     private LedWebSocketHandler webSocketHandler;
 
-
     @PostMapping("/update")
-    public void updateLedStatus(@RequestBody boolean status) throws IOException {
-        webSocketHandler.setLedStatus(status); // Cập nhật trạng thái và gửi cho tất cả các client WebSocket
+    public ResponseEntity updateLedStatus(@RequestBody boolean status, @RequestHeader("Authorization") String token) throws IOException {
+        String usernameReq;
+        String passwordReq ;
+        if (token.contains(":")) {
+            String[] credentials = token.split(":");
+            usernameReq = credentials[0];
+            passwordReq = credentials[1];
+            if(usernameReq.equals("admin") && passwordReq.equals("admin")){
+                webSocketHandler.setLedStatus(status); // Cập nhật trạng thái và gửi cho tất cả các client WebSocket
+                return ResponseEntity.ok().build();
+            }
+        }
+        return ResponseEntity.status(401).build();
     }
 
     @GetMapping("/status")
     @CrossOrigin(origins = "http://127.0.0.1:5500")
-    public boolean getLedStatus() {
-        return webSocketHandler.getLedStatus();
+    public ResponseEntity<Boolean> getLedStatus() {
+        boolean status =webSocketHandler.getLedStatus();
+        return ResponseEntity.ok(status);
     }
 
     @PostMapping("/update-time")
-    public void setLedTime(@RequestBody Map<String, String> timeRequest) throws IOException {
-        LocalDateTime now = LocalDateTime.now();
-        int hour = Integer.parseInt(timeRequest.get("hour"));
-        int minute = Integer.parseInt(timeRequest.get("minute"));
-        String timeString = hour + ":" + minute;
-        webSocketHandler.setLedTime(timeString);
+    public ResponseEntity setLedTime(@RequestBody Map<String, String> timeRequest, @RequestHeader("Authorization") String token) throws IOException {
+        if (token.contains(":")) {
+            String[] credentials = token.split(":");
+            String usernameReq = credentials[0];
+            String passwordReq = credentials[1];
+            if(usernameReq.equals("admin") && passwordReq.equals("admin")) {
+                LocalDateTime now = LocalDateTime.now();
+                int hour = Integer.parseInt(timeRequest.get("hour"));
+                int minute = Integer.parseInt(timeRequest.get("minute"));
+                String timeString = hour + ":" + minute;
+                webSocketHandler.setLedTime(timeString);
+                return ResponseEntity.ok().build();
+            }
+        }
+        return ResponseEntity.status(401).build();
     }
 
     @GetMapping
-    public Led getLedConfig(){
-        return new Led(webSocketHandler.getLedStatus(), webSocketHandler.getLedTime());
+    public ResponseEntity<Led> getLedConfig(){
+        Led led = new Led(webSocketHandler.getLedStatus(), webSocketHandler.getLedTime());
+        return ResponseEntity.ok(led);
     }
 }
